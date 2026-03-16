daa23cf68faddfd00c85c8ff4b51c96ae0049a8f
Shahriar kabir <kabirshahriar468@gmail.com>
2026-03-16 17:35:06 +0600

    temp folder created

---
+++ temp/test1.cpp
@@ -0,0 +1,18 @@
+/*
+ 
+*/
+#include <bits/stdc++.h>
+#define ll long long int
+const int sz = 1e9 + 7;
+const long long linf = 1e18 + 5;
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    ll t=1;
+    cin>>t;
+    while(t--){
+    }
+    return 0;
+}
